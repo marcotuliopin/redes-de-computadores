7002717e9e0c1c1b0e46e6b3a8b7d1d741699eb7
marcotuliopin <marcotufmg@gmail.com>
2024-04-25 16:40:19 -0300

    xesque

--- multi.py
+++ multi.py
@@ -10,12 +10,13 @@ Constant Definitions
 """
 NUM_RIVERS = 4
 NUM_BRIDGES = 8
-MAX_CANNONS = NUM_BRIDGES * NUM_RIVERS
-TIMEOUT = 3.0
-TIMEOUT = 3.0
+NUM_CANNONS = NUM_BRIDGES * NUM_RIVERS
+TIMEOUT = 1.0
 MAX_RETRIES = float('inf')
 
 barrier = threading.Barrier(NUM_RIVERS)
+lock = threading.Lock()
+flag = False
 
 events = [threading.Event() for i in range(NUM_RIVERS)]
 
@@ -23,14 +24,16 @@ for i in range(NUM_RIVERS):
     events.append(threading.Event())
     events[i].clear()
 
-cannon_shot = [False] * MAX_CANNONS
+cannon_shot = [False] * NUM_CANNONS
 
 def play(auth, server_adress):
+    global flag
+    global cannon_shot
     try:
         sock = create_socket(server_adress)
         barrier.wait()
 
-        my_river, status = authenticate(sock, auth)
+        river, status = authenticate(sock, auth)
         if status:
             raise AuthenticationFailedException
         barrier.wait()
@@ -43,25 +46,26 @@ def play(auth, server_adress):
         turn = 0
         gameover = False
         while not gameover:
-            gameover, ships_pre_bridge = pass_turn(sock, auth, turn)
+            gameover, ships = pass_turn(sock, auth, turn)
             if gameover:
                 break
             barrier.wait()
 
-            # TODO: mudar para Lock
-            events[0].set()
-            events[my_river-1].wait()
-            if my_river == 1: # thread 1 is responsible for reseting cannon_shot each round
-                for i in range(len(cannon_shot)):
-                    cannon_shot[i] = False
-            # TODO: paralelizar envio da mensagem
-            shoot(sock, auth, my_river, ships_pre_bridge, cannons)
-            events[my_river-1].clear()
-            if(my_river != NUM_RIVERS):
-                events[my_river].set()
-            turn+=1
+            lock.acquire()
+            if not flag:
+                cannon_shot = [False for _ in range(NUM_CANNONS)]
+            flag = True
+            shots = shoot(river, ships, cannons)
+            lock.release()
+
+            for shot in shots:
+                send_shot(shot[0], shot[1], auth, sock) 
+
             barrier.wait()
-        if(my_river == 1): # thread 1 is responsible for quitting the game
+
+            turn += 1
+            flag = False
+        if (river == 1):
             quit(sock, auth)
     finally:
         sock.close()
@@ -80,7 +84,10 @@ def create_socket(server_adress):
     return sock
 
 def authenticate(sock, auth):
-    data = {'type': 'authreq', 'auth': auth}
+    data = {
+        'type': 'authreq', 
+        'auth': auth
+    }
 
     retries = 0
     while retries <= MAX_RETRIES:
@@ -99,49 +106,63 @@ def authenticate(sock, auth):
     return response['river'], response['status'] 
 
 def place_cannons(sock, auth):
-    data = {'type': 'getcannons', 'auth': auth}
+    data = {
+        'type': 'getcannons', 
+        'auth': auth
+    }
     
     retries = 0
     while retries <= MAX_RETRIES:
         try:
             send(sock, data)
-            # TODO: checar se todas as portas recebem resposta.
             response = receive(sock)
             break
         except (socket.error, socket.timeout) as e:
             print('In cannon placement:')
             print(f'Thread {threading.current_thread} failed with error: {e}')
             retries += 1
-
     if retries > MAX_RETRIES:
         raise CommunicationErrorException
     
-    return response['cannons']
+    cannons = []
+    for cannon in response['cannons']:
+        cannons.append({
+            'bridge': cannon[0],
+            'river': cannon[1]
+        })
+    return cannons
 
 def pass_turn(sock, auth, turn):
-    data = {'type' : 'getturn', 'auth': auth, 'turn': turn }
-    send(sock, data)
-    ships_per_bridge = {}
+    data = {
+        'type' : 'getturn', 
+        'auth': auth, 
+        'turn': turn 
+    }
+
+    ships = {}
     retries = 0
-    for _ in range(NUM_BRIDGES):
-        while retries <= MAX_RETRIES:
-            try:
+    while retries <= MAX_RETRIES:
+        try:
+            send(sock, data)
+            for _ in range(NUM_BRIDGES):
                 response = receive(sock)
                 if(response['type'] == "gameover"):
                     print(response['score'])
                     return True, {}
-                if(response['ships']): #TODO: checar se precisa
-                    ships_per_bridge[response['bridge']] = response['ships']
-                break
-            except (socket.error, socket.timeout) as e:
-                print('In turn:')
-                print('In turn:')
-                print(f'Thread {threading.current_thread} failed with error: {e}')
-                retries += 1
-    return False, ships_per_bridge    
+                if(response['ships']):
+                    ships[response['bridge']] = response['ships']
+            break
+        except (socket.error, socket.timeout) as e:
+            print('In turn:')
+            print(f'Thread {threading.current_thread} failed with error: {e}')
+            retries += 1
+    return False, ships    
         
 def quit(sock, auth):
-    data = {'type' : 'quit', 'auth': auth}
+    data = {
+        'type' : 'quit', 
+        'auth': auth
+    }
     retries = 0
     while retries <= MAX_RETRIES:
         try:
@@ -152,35 +173,63 @@ def quit(sock, auth):
             print(f'Thread {threading.current_thread} failed with error: {e}')
             retries += 1
 
-def shoot(sock, auth, river, ships_per_pridge, cannons):
-    for bridge in ships_per_pridge:
+def shoot(river, ships, cannons):
+    global cannon_shot
+    shots = []
+    for bridge in ships:
         for i in range(len(cannons)):
-            if(cannon_shot[i]):
-                continue
-            # TODO: trocar para dict
-            if cannons[i][0] == bridge and (cannons[i][1] == river or cannons[i][1] == river - 1):
-                send_shot(cannons[i], ships_per_pridge[bridge][0]['id'], auth, sock) # shoots firts element
-                cannon_shot[i] = True
-                break
-            pass
+            for ship in ships[bridge]:
+                if not is_alive(ship):
+                    continue
+                if cannon_shot[i]:
+                    continue
+                if validate_shot(cannons[i], bridge, river):
+                    cannon_shot[i] = True
+                    ship['hits'] += 1
+                    shots.append((cannons[i], ship['id']))
+    return shots
 
-def send_shot(cannon, ship_id, auth, sock):
-    data = {"type": "shot", "auth": auth, "cannon": cannon, "id": ship_id}
+def is_alive(ship):
+    match ship['hull']:
+        case 'frigate':
+            return ship['hits'] < 1
+        case 'destroyer':
+            return ship['hits'] < 2 
+        case 'battleship':
+            return ship['hits'] < 3 
+
+def validate_shot(cannon, bridge, river):
+    if cannon['bridge'] != bridge:
+        return False
+    if cannon['river'] != river:
+        if cannon['river'] == 4:
+            return False
+        if cannon['river'] != river - 1:
+            return False
+    return True
+
+def send_shot(cannon, ship, auth, sock):
+    data = {
+        'type': 'shot',
+        'auth': auth,
+        'cannon': (cannon['bridge'], cannon['river']), 
+        'id': ship
+    }
 
     retries = 0
     while retries <= MAX_RETRIES:
         try:
             send(sock, data)
+            response = receive(sock)
             break
         except (socket.error, socket.timeout) as e:
             print('In shoot')
             print(f'Thread {threading.current_thread} failed with error: {e}')
-        retries += 1
-    response = receive(sock)
+            retries += 1
     if(response['status'] != 0):
         print(f"Shot error (cannon: {cannon}): {response['description']}")
     else:
-        print(f"{cannon} shot {ship_id}")
+        print(f"Cannon {cannon} shot ship {ship}!")
 
 def send(sock, data):
     message = json.dumps(data).encode('utf-8')
@@ -191,7 +240,6 @@ def receive(sock):
     response = json.loads(response)
     if response['type'] == 'gameover'and response['status'] == 1:
         raise InvalidMessageException(response['description'])
-    
     return response
 
 def main():
