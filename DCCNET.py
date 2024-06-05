9753f4ae3a793b879809b8cced47175baa1d5a08
gabrielliman <gabrielliman2002@gmail.com>
2024-06-05 20:26:50 -0300

    created funcions pack and unpack

--- DCCNET.py
+++ DCCNET.py
@@ -4,6 +4,8 @@ import struct
 class DCCNET:
     def __init__(self,host,port): #gabriel
         self.TIMEOUT=3
+        self.ID=0
+        self.SYNC=0xDCC023C2
         #cria socket para conexao e salva para acesso nas funcoes
         try:
             self.sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
@@ -12,25 +14,35 @@ class DCCNET:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             self.sock.connect((host,port))
         self.sock.settimeout(self.TIMEOUT)
-        self.ID=0
-        self.SYNC=0xDCC023C2
+
     
     #quando o dado tiver mais de 2^16 bits dividimos em dois frames
     #quando confirmar recebimento trocar o id
 
     #flag no geral sera enviada como zero mas quando for o ultimo frame de dado sera 6
+
+    #to assumindo que a data vai ser mandada como uma string, caso a gnt passe direto como bytes é so tirar a linha com encode 
     def pack(self,data,flag): #gabriel
-        #H: length, checksum,ID
-        #I: SYNC
 
         # Definindo os campos do frame
+        data=data.encode('ascii')
         length = len(data)
 
         # Empacotar SYNC, ID e Length em big-endian
         message = struct.pack(f'!IIHHHB{length}s', self.SYNC,self.SYNC, self.checksum(data), length, self.ID,flag,data)
+        return message
     
+
+    #to retornando a data como string e deixando a identificação e tratamento de erros para fora da função
     def unpack(self,message): #gabriel
-        pass
+        offset=0
+        sync1,sync2,checksum,length,id,flag=struct.unpack_from("!IIHHHB",message,offset)
+        offset+=struct.calcsize('!IIHHHB')
+        data=struct.unpack_from(f"!{length}s",message,offset)[0]
+        data=data.decode('ascii')
+        return sync1,sync2,checksum,length,id,flag,data
+    
+
     def receive(): #araju
         pass
     def send(): #marco
