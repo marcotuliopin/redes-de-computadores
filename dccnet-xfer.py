0fe0194443f79b30a1317f9c648554ff7f392174
marcotuliopin <marcotufmg@gmail.com>
2024-06-09 16:23:00 -0300

    changes to xfer

--- dccnet-xfer.py
+++ dccnet-xfer.py
@@ -6,53 +6,42 @@ from DCCNET import DCCNET
 def comm(dccnet: DCCNET, sock, input, output):
     dccnet.sock = sock
     has_finished_receiving = False
-    input_file = open(input, 'r')
 
-    lines = input_file.readlines()
-    for line in lines:
-        fsize = 2**16
-        fsize //= 8
-        frames = [line[i: i + fsize] for i in range(len(0, line, fsize))]
+    with open(input, 'r') as f:
+        buffer = f.read()
 
-        for frame in frames:
-            while True:
-                dccnet.send_frame(frame)
-                data, flag, id, checksum = dccnet.recv_frame()
+    fsize = 2**16
+    fsize //= 8
+    frames = [buffer[i: i + fsize] for i in range(len(0, buffer, fsize))]
 
-                if flag & dccnet.FLAG_ACK and id == dccnet.id_send:
-                    if flag & dccnet.FLAG_END:
-                        raise dccnet.invalid_flag
-                    if data:
-                        raise dccnet.invalid_payload
-                    dccnet.id_send ^= 1
-                    break
+    for i in range(len(frames)):
+        frame = frames[i]
+        if i == len(frames) - 1:
+            flag = dccnet.FLAG_END
+        else:
+            flag = dccnet.FLAG_EMPTY
 
-                else:
-                    if flag & dccnet.FLAG_END:
-                        has_finished_receiving = True
-                    elif not data:
-                        raise dccnet.invalid_payload
-                    
-                    if id != dccnet.id_recv:
-                        if data:
-                            with open(output, 'w') as out:
-                                out.write(data)
-                        dccnet.send_frame(data=None, flag=dccnet.FLAG_ACK)
-                        dccnet.id_recv ^= 1
+        while True:
+            dccnet.send_frame(frame, flag)
+            data, flag, id, checksum = dccnet.recv_frame()
 
-                    elif id == dccnet.id_recv and checksum == dccnet.last_checksum:
-                        dccnet.send_frame(data=None, flag=dccnet.FLAG_ACK)
-                    
-                    elif id == dccnet.ID_RESET and flag == dccnet.FLAG_RESET:
-                        dccnet.sock.close()
-                        raise dccnet.reset()
-    input_file.close()
+            # Receiving ACK from sent file
+            if flag & dccnet.FLAG_ACK and id == dccnet.id_send:
+                if flag & dccnet.FLAG_END:
+                    raise dccnet.invalid_flag
+                if data:
+                    raise dccnet.invalid_payload
+                dccnet.id_send ^= 1
+                break
+            # Receiving data from external file
+            else:
+                has_finished_receiving = recv_file(dccnet, flag, data, 
+                                                checksum, output, has_finished_receiving)
 
     while not has_finished_receiving:
         data, flag, id, checksum = dccnet.recv_frame()
         has_finished_receiving = recv_file(dccnet, flag, data, 
                                            checksum, output, has_finished_receiving)
-
     dccnet.sock.close()
         
 
