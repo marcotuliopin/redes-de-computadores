1ec34f3d0dc13bfc96b2a87fb8652ecee00c9c04
marcotuliopin <marcotufmg@gmail.com>
2024-06-09 15:32:59 -0300

    communication definition

--- dccnet-xfer.py
+++ dccnet-xfer.py
@@ -1,3 +1,4 @@
+import socket
 import sys
 import threading
 from DCCNET import DCCNET
@@ -9,30 +10,68 @@ def receive_file(receiver, output):
 
     pass
 
-def send_file(sender, input):
-    with open(input, 'r') as f:
-        data = f.read()
-        sender.sendall(data)
+def comm(dccnet: DCCNET, input, output):
+    has_finished_receiving = False
+    f = open(input, 'r')
+
+    lines = f.readlines()
+    for line in lines:
+        fsize = 2**16
+        fsize //= 8
+        frames = [line[i: i + fsize] for i in range(len(0, line, fsize))]
+
+        for frame in frames:
+            while True:
+                dccnet.send_frame(frame)
+                data, flag, id, checksum = dccnet.recv_frame()
+
+                if flag & dccnet.FLAG_ACK and id == dccnet.id_send:
+                    if flag & dccnet.FLAG_END:
+                        raise dccnet.invalid_flag
+                    if data:
+                        raise dccnet.invalid_payload
+                    dccnet.id_send ^= 1
+                    break
+
+                else:
+                    if flag & dccnet.FLAG_END:
+                        has_finished_receiving = True
+                    elif not data:
+                        raise dccnet.invalid_payload
+                    
+                    if id != dccnet.id_recv:
+                        if data:
+                            with open(output, 'w') as out:
+                                out.write(data)
+                        dccnet.send_frame(data=None, flag=dccnet.FLAG_ACK)
+                        dccnet.id_recv ^= 1
+
+                    elif id == dccnet.id_recv and checksum == dccnet.last_checksum:
+                        dccnet.send_frame(data=None, flag=dccnet.FLAG_ACK)
+                    
+                    elif id == dccnet.ID_RESET and flag == dccnet.FLAG_RESET:
+                        dccnet.sock.close()
+    
+    f.close()
     pass
 
 def main():
     _, mode, *params = sys.argv
 
+    receiver = DCCNET()
+
     if mode == '-s':
-        port, input, output = params
-        sender = DCCNET(port, mode=0)
-        receiver = DCCNET(port, mode=0)
+        pass
     else:
         host, input, output = params
         ip, port = host.split(sep=':')
-        sender = DCCNET(port, mode=1, host=ip)
-        receiver = DCCNET(port, mode=1, host=ip)
 
-    receiving_t = threading.Thread(target=receive_file, args=(receiver, output))
-    sending_t = threading.Thread(target=send_file, args=(sender, input))
-    try:
-        receiving_t.start()
-        sending_t.start()
-    finally:
-        receiving_t.join()
-        sending_t.join()
+        try:
+            sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+            sock.connect((ip,port))
+        except socket.error:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.connect((ip,port))
+        sock.settimeout(1)
+        dccnet = DCCNET(sock)
+        comm(dccnet, input, output)
