605c5c026648f6736aaf02c92aa5baf01b16178d
marcotuliopin <marcotufmg@gmail.com>
2024-06-09 11:18:14 -0300

    changes to xfer

--- dccnet-xfer.py
+++ dccnet-xfer.py
@@ -2,24 +2,6 @@ import sys
 import threading
 from DCCNET import DCCNET
 
-def system_mode(port, input, output):
-    sender = DCCNET()
-    receiver = DCCNET()
-
-def client_mode(ip, port, input, output):
-    sender = DCCNET(ip, port)
-    receiver = DCCNET(ip, port)
-    receiving_t = threading.Thread(target=receive_file, args=(receiver, output))
-    sending_t = threading.Thread(target=send_file, args=(sender, input))
-
-    try:
-        receiving_t.start()
-        sending_t.start()
-    finally:
-        receiving_t.join()
-        sending_t.join()
-
-
 def receive_file(receiver, output):
     data = receiver.recvall()
     with open(output, 'w') as f:
@@ -38,9 +20,19 @@ def main():
 
     if mode == '-s':
         port, input, output = params
-        system_mode(port, input, output)
+        sender = DCCNET(port, mode=0)
+        receiver = DCCNET(port, mode=0)
     else:
         host, input, output = params
         ip, port = host.split(sep=':')
-        client_mode(ip, port, input, output)
+        sender = DCCNET(port, mode=1, host=ip)
+        receiver = DCCNET(port, mode=1, host=ip)
 
+    receiving_t = threading.Thread(target=receive_file, args=(receiver, output))
+    sending_t = threading.Thread(target=send_file, args=(sender, input))
+    try:
+        receiving_t.start()
+        sending_t.start()
+    finally:
+        receiving_t.join()
+        sending_t.join()
