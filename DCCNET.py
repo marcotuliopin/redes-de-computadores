06a5d52993203f395db2a9540b667e1f40a1d168
raularaju <raularaju@uol.com.br>
2024-06-24 06:59:19 -0300

    md5 file working

--- DCCNET.py
+++ DCCNET.py
@@ -37,8 +37,7 @@ class DCCNET:
         length = len(data)
         aux = struct.pack(f'!IIHHHB{length}s', self.SYNC, self.SYNC, 0, length, id, flag, data)
         frame = struct.pack(f'!IIHHHB{length}s', self.SYNC, self.SYNC, self.checksum(aux), length, id, flag, data)
-        print("ENVIADO")
-        print(f"flag sent: {flag:x} == {flags[flag]}, length sent: {length}, id sent: {id:x}, checksum sent: {self.checksum(aux):x}, data sent: {data}") 
+        print(f"SEN: flag: {flag:x} == {flags[flag]}, length: {length}, id: {id:x}, checksum: {self.checksum(aux):x}, data: {data}") 
         print()
         return frame
     
@@ -62,13 +61,13 @@ class DCCNET:
                 else:
                     sync_count = 0
                 a += 1
-                if a > 6:
-                    raise KeyboardInterrupt
+                # comentei aqui pq tava dando pau no md5
+                """ if a > 6:
+                    raise KeyboardInterrupt """
 
             header = self.sock.recv(self.HEADER_SIZE - 2*self.SYNC_SIZE)
         except socket.timeout:
             return None, None, None, None
-        print('Received response')
 
         checksum, length, id, flag = struct.unpack('!HHHB', header)
         data = self.sock.recv(length)
@@ -79,8 +78,8 @@ class DCCNET:
 
         data = data.decode('ascii')
 
-        print("RECEBIDO:")
-        print(f"flag recv: 0x{flag:x} == {flags[flag]}, length recv: {length}, id recv: 0x{id:x}, checksum recv: 0x{recv_checksum:x}, data recv: {data}")
+        print(f"REC: flag: {flag:x} == {flags[flag]}, length: {length}, id: {id:x}, checksum: {recv_checksum:x}, data: {data}")
+        print()
         return data, flag, id, checksum
 
 
--- dccnet-md5.py
+++ dccnet-md5.py
@@ -4,8 +4,14 @@ import threading
 import time
 import hashlib
 from DCCNET import DCCNET
-#python dccnet-md5.py 150.164.213.245:51555
-GAS = "2021032110:1:fd01eed7baa1eb0a3c06480a303c94cfa2b54e34045a307683e88c69e37955d2+3b3ccb9597ef0e57f427b0cde3ae23f1498b7e25c11a639aaefe45ef0eed32d6\n"
+#python dccnet-md5.py 150.164.213.245:51555 2021032110:1:fd01eed7baa1eb0a3c06480a303c94cfa2b54e34045a307683e88c69e37955d2+3b3ccb9597ef0e57f427b0cde3ae23f1498b7e25c11a639aaefe45ef0eed32d6
+
+flags={
+        0x80:"FLAG_ACK",
+        0x40 :"FLAG_END",
+        0x00 :"FLAG_EMPTY",
+        0x20 :"FLAG_RESET"
+}
 
 def md5_checksum(input_string: str) -> str: 
     # Create an md5 hash object
@@ -15,7 +21,7 @@ def md5_checksum(input_string: str) -> str:
     md5_hash.update(input_string.encode('ascii'))
     
     # Return the hexadecimal digest of the hash
-    return md5_hash.hexdigest()
+    return md5_hash.hexdigest() + "\n"
 
 """
 A frame can only be accepted if it is an acknowledgement frame for the last transmitted frame; 
@@ -27,7 +33,7 @@ or a reset frame.
 def send_checksum(dccnet: DCCNET, message : str, last_id : int, last_check_sum : int): # send checksum and receive ack
       payload = md5_checksum(message)
       while True:
-            dccnet.send_frame(payload)
+            dccnet.send_frame(payload, flag= dccnet.FLAG_EMPTY, id = dccnet.id_send)
             data, flag, id, checksum =  dccnet.recv_frame()
             if flag & dccnet.FLAG_RESET:
                   raise Exception(f"{data}")
@@ -35,11 +41,13 @@ def send_checksum(dccnet: DCCNET, message : str, last_id : int, last_check_sum :
                  dccnet.id_send ^= 1
                  break
             elif id == last_id and checksum == last_check_sum: # retrasmission of last frame
-                  dccnet.send_frame(None, flag=dccnet.FLAG_ACK)
+                  dccnet.send_frame(None, flag=dccnet.FLAG_ACK, id=last_id)
+            elif id == last_id ^ 1: # endpoint lost ack
+                  dccnet.send_frame(None, flag=dccnet.FLAG_ACK, id=dccnet.id_recv)
 
-def authenticate(dccnet: DCCNET):
+def authenticate(dccnet: DCCNET, gas):
       while True:
-            dccnet.send_frame(GAS)
+            dccnet.send_frame(gas)
             data, flag, id, _ = dccnet.recv_frame()
             if flag & dccnet.FLAG_RESET:
                   raise Exception(f"{data}")
@@ -49,38 +57,17 @@ def authenticate(dccnet: DCCNET):
 
 
 
-def test(dccnet: DCCNET, sock):
-      dccnet.sock = sock
-      authenticate(dccnet)
-      n_frames = 0
-      while True:
-            print("Trying to receive ...")
-            data, flag, id, checksum = dccnet.recv_frame() 
-            if flag & dccnet.FLAG_RESET:
-                  raise Exception(f"{data}")
-            if flag == dccnet.FLAG_EMPTY:
-                  dccnet.send_frame(data = None, flag = dccnet.FLAG_ACK, id=id)
-                  if id == dccnet.id_recv:
-                        continue
-            if flag & dccnet.FLAG_END:
-                  break  
-      
-
-
-
-def comm(dccnet: DCCNET, sock):
+def comm(dccnet: DCCNET, sock, gas):
       dccnet.sock = sock 
       # Authentication
-      authenticate(dccnet)
+      authenticate(dccnet, gas)
       curr_msg = ""
-      n_frames = 0
       while True:
             while True: # Tries to receive a data frame
                   data, flag, id, checksum = dccnet.recv_frame() 
                   if flag & dccnet.FLAG_RESET:
                         raise Exception(f"{data}")
-                  if flag == dccnet.FLAG_EMPTY:
-                        print(f"n_frames : {n_frames}")
+                  elif flag == dccnet.FLAG_EMPTY or flag & dccnet.FLAG_END:
                         dccnet.send_frame(data = None, flag = dccnet.FLAG_ACK, id=id)
                         if id == dccnet.id_recv:
                               continue
@@ -89,28 +76,22 @@ def comm(dccnet: DCCNET, sock):
                         curr_msg += split_data[0]
                         if '\n' in data:
                               send_checksum(dccnet, curr_msg, id, checksum)
-                        print("PASSOU 1")
                         for i in range(1, len(split_data) - 1):
                               if(split_data[i] != ''):
-                                    send_checksum(dccnet, split_data[i])
-                        print("PASSOU 2")
+                                    send_checksum(dccnet, split_data[i], id, checksum)
                         curr_msg = split_data[-1]
                         break
 
             if flag & dccnet.FLAG_END:
                   break  
-      """"
-      thread para receber linhas:
 
-      thread para mandar linhas:
-      """      
 
 
 
 def main():
-      _ , server  = sys.argv
+      _ , server, gas  = sys.argv
       ip, port = server.split(sep=':')
-      
+      gas += '\n'
       try:
           print(f"Connected to: {ip}:{port}\n\n")
           sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
@@ -120,8 +101,8 @@ def main():
           sock.connect((ip, int(port)))
       sock.settimeout(100)
       dccnet = DCCNET()
-      test(dccnet, sock)
-      #comm(dccnet, sock)
+      #test(dccnet, sock)
+      comm(dccnet, sock, gas)
 
 if __name__ == '__main__':
     main()
